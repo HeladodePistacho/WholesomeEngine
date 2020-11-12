c17e1eec3479700034b69b25f268a01422728b73
HeladodePistacho <th_sola@hotmail.com>
2020-11-12 16:16:06 +0100

    Added LOG utility

---
+++ WholesomeEngine/WholesomeEngine/Globals.h
@@ -0,0 +1,34 @@
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <sstream>
+
+inline std::stringstream out;
+
+template<typename T, typename... Params>
+void LOG(std::string str, T arg)
+{
+	if constexpr(std::is_null_pointer_v<T>)
+		out << str << std::endl;
+	else
+		out << str.substr(0, str.find_first_of('%')) << arg << std::endl;
+
+	OutputDebugString(out.str().c_str());
+	out.clear();
+	out.str(std::string());
+}
+
+template<typename T, typename... Params>
+void LOG(std::string str, T arg, Params... args)
+{
+	out << str.substr(0, str.find_first_of('%')) << arg;
+	LOG(str.substr(str.find_first_of('%') + 1), args...);
+}
+
+#endif // !_GLOBALS_H_
+
+
+
--- WholesomeEngine/WholesomeEngine/main.cpp
+++ WholesomeEngine/WholesomeEngine/main.cpp
@@ -1,8 +1,21 @@
+#include "Engine.h"
+#include <memory>
+#include "Globals.h"
 
 
 int main()
 {
+	//std::unique_ptr<Engine> app;
 
+	LOG("TMP % lololol % xs % ", 1, 2.8f, 3.1);
+	LOG("--------------- TEST -------------", nullptr);
+
+	LOG("Hola soy: % y tengo % sidas", "Eric", 12, nullptr);
+
+	std::string omega_lol{ "OMEEEGA LOOOOL" };
+	LOG(omega_lol, nullptr);
+
+	getchar();
 
 	return 0;
 }

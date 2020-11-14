ad5fb6f065f374bdde98dbe8ad5355f675cf4f70
HeladodePistacho <th_sola@hotmail.com>
2020-11-14 13:46:03 +0100

    Added Module Window

--- WholesomeEngine/WholesomeEngine/Module.h
+++ WholesomeEngine/WholesomeEngine/Module.h
@@ -1,11 +1,19 @@
 #ifndef _MODULE_H_
 #define _MODULE_H_
+#include "Globals.h"
 
 class Module
 {
 public:
 	Module();
 	virtual ~Module();
+
+	virtual ENGINE_STATUS Init()       {};
+	virtual ENGINE_STATUS Start()      {};
+	virtual ENGINE_STATUS PreUpdate()  {};
+	virtual ENGINE_STATUS Update()     {};
+	virtual ENGINE_STATUS PostUpdate() {};
+	virtual ENGINE_STATUS CleanUp()    {};
 };
 
 
---
+++ WholesomeEngine/WholesomeEngine/ModuleWindow.h
@@ -0,0 +1,15 @@
+#ifndef _MODULE_WINDOW_H_
+#define _MODULE_WINDOW_H_
+
+#include "Module.h"
+
+class ModuleWindow : public Module
+{
+public:
+	ModuleWindow();
+	~ModuleWindow();
+
+private:
+};
+
+#endif
